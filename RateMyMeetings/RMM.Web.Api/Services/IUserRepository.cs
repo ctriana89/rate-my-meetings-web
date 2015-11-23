7b7ce28e16d74c922e9e7e60fe104e55a7adc9c5
Corali Triana <an4may39@gmail.com>
2015-11-22 18:17:42 -0800

    Adding CreateUser Method

--- RateMyMeetings/RMM.Web.Api/Services/IUserRepository.cs
+++ RateMyMeetings/RMM.Web.Api/Services/IUserRepository.cs
@@ -10,5 +10,8 @@ namespace RMM.Web.Api.Services
     public interface IUserRepository
     {
         User GetUser(string user, string password);
+
+        User GetUser(string user);
+        User CreateUser(string user, string company, string password);
     }
 }
