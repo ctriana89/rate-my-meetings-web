d81d6955b1e6601692ca45f542b66fc0e3f7da89
Corali Triana <an4may39@gmail.com>
2015-11-22 18:16:13 -0800

    Extracting EmailValidator

---
+++ RateMyMeetings/RMM.Web.Api/Controllers/EmailValidator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace RMM.Web.Api
+{
+    public interface IEmailValidator
+    {
+        bool IsValidEmail(string email);
+    }
+
+    public class EmailValidator : IEmailValidator
+    {
+        public bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email,
+                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+    }
+}
